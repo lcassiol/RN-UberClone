0bf11854a47ba57f2f6af23006e9b178ebf2aee5
Cassio C <lccassiol@gmail.com>
2020-02-07 15:52:41 -0300

    Create project

---
+++ src/components/Map/index.js
@@ -0,0 +1,144 @@
+import React, {Component} from 'react';
+import {View, Image} from 'react-native';
+import MapView, {Marker} from 'react-native-maps';
+import {getPixelSize} from '../../utils';
+import Geocoder from 'react-native-geocoding';
+import Geolocation from 'react-native-geolocation-service';
+
+import Search from '../Search';
+import Directions from '../Directions';
+import Details from '../Details';
+
+import markerImage from '../../assets/marker.png';
+import backImage from '../../assets/back.png';
+
+import {
+  LocationBox,
+  LocationText,
+  LocationTimeBox,
+  LocationTimeText,
+  LocationTimeTextSmall,
+  Back,
+} from './styles';
+
+Geocoder.init('AIzaSyClWoW26mY9P56LPjgFV6VS7ufv3eBxut0');
+
+export default class Map extends Component {
+  state = {
+    region: null,
+    destination: null,
+    duration: null,
+    location: null,
+  };
+
+  async componentDidMount() {
+    Geolocation.getCurrentPosition(
+      async ({coords: {latitude, longitude}}) => {
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134,
+          },
+        });
+
+        const response = await Geocoder.from({latitude, longitude});
+        const address = response.results[0].formatted_address;
+        const location = address.substring(0, address.indexOf(','));
+
+        this.setState({
+          location,
+        });
+      }, // sucesso
+      error => {
+        console.log(error);
+      }, // erro
+      {
+        timeout: 2000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      },
+    );
+  }
+
+  handleLocationSelected = (data, {geometry}) => {
+    const {
+      location: {lat: latitude, lng: longitude},
+    } = geometry;
+
+    this.setState({
+      destination: {
+        latitude,
+        longitude,
+        title: data.structured_formatting.main_text,
+      },
+    });
+  };
+
+  handleBack = () => {
+    this.setState({destination: null});
+  };
+
+  render() {
+    const {region, destination, duration, location} = this.state;
+
+    return (
+      <View style={{flex: 1}}>
+        <MapView
+          style={{flex: 1}}
+          region={region}
+          showsUserLocation
+          loadingEnabled
+          ref={el => (this.mapView = el)}>
+          {destination && (
+            <>
+              <Directions
+                origin={region}
+                destination={destination}
+                onReady={result => {
+                  this.setState({duration: Math.floor(result.duration)});
+                  this.mapView.fitToCoordinates(result.coordinates, {
+                    edgePadding: {
+                      right: getPixelSize(50),
+                      left: getPixelSize(50),
+                      top: getPixelSize(50),
+                      bottom: getPixelSize(350),
+                    },
+                  });
+                }}
+              />
+              <Marker
+                coordinate={destination}
+                anchor={{x: 0, y: 0}}
+                image={markerImage}>
+                <LocationBox>
+                  <LocationText>{destination.title}</LocationText>
+                </LocationBox>
+              </Marker>
+              <Marker coordinate={region} anchor={{x: 0, y: 0}}>
+                <LocationBox>
+                  <LocationTimeBox>
+                    <LocationTimeText>{duration}</LocationTimeText>
+                    <LocationTimeTextSmall>MIN</LocationTimeTextSmall>
+                  </LocationTimeBox>
+                  <LocationText>{location}</LocationText>
+                </LocationBox>
+              </Marker>
+            </>
+          )}
+        </MapView>
+        {destination ? (
+          <>
+            <Back onPress={this.handleBack}>
+              <Image source={backImage} />
+            </Back>
+            <Details />
+          </>
+        ) : (
+          <Search onLocationSelected={this.handleLocationSelected} />
+        )}
+      </View>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import Map from './components/Map';
+
+const App = () => <Map />;
+
+export default App;
